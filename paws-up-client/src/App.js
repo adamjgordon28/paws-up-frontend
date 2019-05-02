9087117aa5b02bcec2ed8022b7629793654093f2
adamjgordon28 <adamjgordon24@gmail.com>
2019-05-02 11:22:46 -0400

    created react frontend

---
+++ paws-up-client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Hello!
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
