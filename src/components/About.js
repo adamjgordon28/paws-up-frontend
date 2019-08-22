0d8de1f8928a69d701fadb10e1963c48548911d8
adamjgordon28 <adamjgordon24@gmail.com>
2019-08-22 15:49:30 -0400

    changed about description testing update protocol

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
   <div className="ui message" style={{width: "30%",color: "black"}} >
     <h1>About PawsUp</h1>
 
-    <p>PawsUp is an organization dedicated to helping prospective pet adopters meet the pet of their dreams. With all of the pets that need adopting, it can be overwhelming to determine where to start - that's why PawsUp is here to help! Feel free to browse through our pets that need adopting, and set up a meeting if things feels right! </p>
+    <p>PawsUp is an organization dedicated to helping prospective pet adopters meet the pet of their dreams. With all of the pets that need adopting, it can be overwhelming to decide where to start - that's why PawsUp is here to help! Feel free to browse through our pets that need adopting, and set up a meeting if things feels right! </p>
 
   </div>
 
